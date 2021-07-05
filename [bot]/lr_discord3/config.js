d3f43c19a18147c6347d278ec40d041b30d6ee64
Stable <55439243+xenfovn@users.noreply.github.com>
2021-07-05 10:45:21 +0700

    BOT DISCORD
    THAY ĐỔI API

--- [bot]/lr_discord3/config.js
+++ [bot]/lr_discord3/config.js
@@ -1,4 +1,4 @@
 ﻿var config = {};
-config.channelId = '853047374074085399'; //id của kênh sử dụng bot
-config.BotToken = 'ODMzNjM2NTA5MTcyMTA1MjM5.YH1OgA.Li4n9Tswm-oZaih9s9jHhUwJKnk'; //token của bot
+config.channelId = 'IDCOMMANDROOM'; //id của kênh sử dụng bot
+config.BotToken = 'TOKEN'; //token của bot
 module.exports = config;
